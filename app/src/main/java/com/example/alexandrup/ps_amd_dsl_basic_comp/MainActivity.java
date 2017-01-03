575fc0a7eb15eb80877a0807893d4dc145e9842c
PerryB52 <alex2pereteanu@gmail.com>
2017-01-03 18:57:31 +0200

    Commit 3 - SnackBar + Coordinator Layout

--- app/src/main/java/com/example/alexandrup/ps_amd_dsl_basic_comp/MainActivity.java
+++ app/src/main/java/com/example/alexandrup/ps_amd_dsl_basic_comp/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.alexandrup.ps_amd_dsl_basic_comp;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CoordinatorLayout coordinatorLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorlayout);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -20,6 +26,12 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "FAB Clicked", Toast.LENGTH_SHORT).show();
             }
         });
+
+    }
+
+    public void showSimpleSnackBar(View view) {
+
+        Snackbar.make(coordinatorLayout, "Hello from simple SnackBar", Snackbar.LENGTH_SHORT).show();
 
     }
 }
