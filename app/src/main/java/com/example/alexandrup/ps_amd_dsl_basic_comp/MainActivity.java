29b14e3a662960034f36a305f70f5c99e986b07b
PerryB52 <alex2pereteanu@gmail.com>
2017-01-03 18:45:15 +0200

    Adding Floating Action Button

--- app/src/main/java/com/example/alexandrup/ps_amd_dsl_basic_comp/MainActivity.java
+++ app/src/main/java/com/example/alexandrup/ps_amd_dsl_basic_comp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.alexandrup.ps_amd_dsl_basic_comp;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "FAB Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
