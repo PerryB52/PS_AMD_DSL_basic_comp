465884e0d9035f6420276b2838192449090fdd56
PerryB52 <alex2pereteanu@gmail.com>
2017-01-03 19:12:08 +0200

    Commit 5 - Customized SnackBar

--- app/src/main/java/com/example/alexandrup/ps_amd_dsl_basic_comp/MainActivity.java
+++ app/src/main/java/com/example/alexandrup/ps_amd_dsl_basic_comp/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.alexandrup.ps_amd_dsl_basic_comp;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -45,6 +48,35 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(coordinatorLayout, "File Loaded Succesfully", Snackbar.LENGTH_SHORT).show();
             }
         });
+
+        snackbar.show();
+
+    }
+
+    public void showSnackBarWithColoredText(View view) {
+
+        Snackbar snackbar = Snackbar.make(coordinatorLayout, "Error Loading File", Snackbar.LENGTH_LONG);
+        snackbar.setAction("RETRY", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //do something
+
+            }
+        });
+
+        View snackBarLayout = snackbar.getView();
+        snackBarLayout.setBackgroundColor(Color.BLUE);
+
+        //snackbar message text color
+        TextView txvMessage = (TextView) snackBarLayout.findViewById(android.support.design.R.id.snackbar_text);
+        txvMessage.setTextColor(Color.YELLOW);
+
+        //changing action button color - with in built method 1
+        //snackbar.setActionTextColor(Color.GREEN);
+
+        //changing action button color - method 2
+        Button btn = (Button) snackbar.getView().findViewById(android.support.design.R.id.snackbar_action);
+        btn.setTextColor(Color.RED);
 
         snackbar.show();
 
